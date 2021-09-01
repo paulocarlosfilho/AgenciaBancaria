cfdd54dddfc50c4ec3d184880f29822ab4455c4d
paulocarlosfilho <paulocarlosfilho@gmail.com>
2021-09-01 14:04:16 -0300

    Adicionar arquivos de projeto.

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,36 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua teste", "384759387", "Paulista", "Pernambuco");
+                Cliente cliente = new Cliente("Paulo", "123456", "987234", endereco);
+
+                ContaCorrente conta = new ContaCorrente(cliente, 100);
+
+                Console.WriteLine("Conta " + conta.Situacao + " - " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                string senha = "abc123456789";
+
+                conta.Abrir(senha);
+
+                Console.WriteLine("Conta " + conta.Situacao + " - " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+
+                conta.Sacar(10, senha);
+
+                Console.WriteLine("Saldo: R$" + conta.Saldo);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ AgenciaBancaria.Dominio/ContaPoupaca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgenciaBancaria.Dominio
+{
+    public class ContaPoupaca : ContaBancaria
+    {
+        public ContaPoupaca(Cliente cliente): base(cliente)
+        {
+            PercentualRendimento = 0.003M;
+        }
+        public decimal PercentualRendimento { get; private set; }
+    }
+}
